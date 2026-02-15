23513d2b0ee81282951703a20d59b64cd9c3e155
mellowboyXD <chamerallyishfaq@gmail.com>
2026-02-15 12:43:32 -0500

    Shift argv by one to handle command input

--- src/nbc.c
+++ src/nbc.c
@@ -16,31 +16,31 @@
 			(da)->cap = INIT_CAP;                                  \
 			(da)->items = malloc(INIT_CAP * sizeof(*(da)->items)); \
 		}                                                              \
-		if ((da)->count == (da)->cap) {                                \
+		if ((da)->size == (da)->cap) {                                 \
 			(da)->cap *= 2;                                        \
 			(da)->items =                                          \
 				realloc((da)->items,                           \
 					(da)->cap * sizeof(*(da)->items));     \
 		}                                                              \
 		assert((da)->items != NULL);                                   \
-		(da)->items[(da)->count++] = (item);                           \
+		(da)->items[(da)->size++] = (item);                            \
 	} while (0)
 
 #define da_free(da) /* frees the dynamic array */ \
 	do {                                      \
 		(da)->cap = 0;                    \
-		(da)->count = 0;                  \
+		(da)->size = 0;                   \
 		free((da)->items);                \
 	} while (0)
 
 typedef struct {
 	size_t cap;
-	size_t count;
+	size_t size;
 	char *items;
 } string_builder; /* size: 32 bytes */
 
 typedef struct {
-	size_t count;
+	size_t size;
 	const char *cstr;
 } string_view; /* size: 16 bytes */
 
@@ -50,8 +50,8 @@ typedef struct {
 } word_freq; /* size: 20 bytes */
 
 typedef struct {
-	size_t count;
 	size_t cap;
+	size_t size;
 	word_freq *items;
 } word_freqs; /* size: 32 bytes */
 
@@ -60,7 +60,7 @@ bool read_entire_file(const char *filepath, string_builder *sb)
 {
 	FILE *fp = fopen(filepath, "r");
 	if (!fp) {
-		fprintf(stderr, "error: could not open file %s\n", filepath);
+		fprintf(stderr, "error: could not open file '%s'\n", filepath);
 		return false;
 	}
 
@@ -77,10 +77,10 @@ bool read_entire_file(const char *filepath, string_builder *sb)
 string_view trim_left(string_view sv)
 {
 	size_t i = 0;
-	while (i < sv.count && isspace(sv.cstr[i]))
+	while (i < sv.size && isspace(sv.cstr[i]))
 		i++;
 
-	string_view result = { .count = sv.count - i, .cstr = sv.cstr + i };
+	string_view result = { .size = sv.size - i, .cstr = sv.cstr + i };
 
 	return result;
 }
@@ -89,41 +89,62 @@ string_view trim_left(string_view sv)
 string_view chop_by_space(string_view *sv)
 {
 	size_t i = 0;
-	while (i < sv->count && !isspace(sv->cstr[i]))
+	while (i < sv->size && !isspace(sv->cstr[i]))
 		i++;
 
-	string_view result = { .count = i, .cstr = sv->cstr };
+	string_view result = { .size = i, .cstr = sv->cstr };
 
-	if (i < sv->count) {
-		sv->count -= i + 1;
+	if (i < sv->size) {
+		sv->size -= i + 1;
 		sv->cstr += i + 1;
 	} else {
-		sv->count -= i;
+		sv->size -= i;
 		sv->cstr += i;
 	}
 
 	return result;
 }
 
-word_freq *find_key(word_freqs *frqs, string_view key)
+/* find_key: returns a pointer to the word_freq (key-value pair) from the 
+ * 		dynamic array word_freqs */
+word_freq *find_key(word_freqs *freqs, string_view key)
 {
-	for (size_t i = 0; i < frqs->count; i++) {
-		if (strncmp(frqs->items[i].word.cstr, key.cstr, key.count) == 0)
-			return &frqs->items[i];
+	for (size_t i = 0; i < freqs->size; i++) {
+		if (strncmp(freqs->items[i].word.cstr, key.cstr, key.size) == 0)
+			return &freqs->items[i];
 	}
 	return NULL;
 }
 
-int main()
+char *shift_argv(char **argv, int *argc)
 {
-	string_builder buf = { 0 };
-	if (!read_entire_file("data/enron1/Summary.txt", &buf))
+	static int i = 0;
+	if (*argc < i)
+		return NULL;
+
+	(*argc)--;
+	char *ret = argv[i++];
+	return ret;
+}
+
+int main(int argc, char **argv)
+{
+	const char *program_name = shift_argv(argv, &argc);
+	if (argc < 1) {
+		fprintf(stderr, "Usage: %s <input-file>.txt\n", program_name);
 		return 1;
-	string_view content = { .count = buf.count, .cstr = buf.items };
+	}
+
+	const char *filepath = shift_argv(argv, &argc);
+	string_builder buf = { 0 };
+	if (!read_entire_file(filepath, &buf))
+		return 1;
+	string_view content = { .size = buf.size, .cstr = buf.items };
+	unsigned total_count = 0;
 
 	word_freqs freqs = { 0 };
 
-	for (size_t i = 0; i < content.count; i++) {
+	for (size_t i = 0; i < content.size; i++) {
 		content = trim_left(content);
 		string_view token = chop_by_space(&content);
 		//printf("|%.*s|\n", (int)token.count, token.data);
@@ -134,12 +155,15 @@ int main()
 			word_freq f = { .count = 1, .word = token };
 			da_append(&freqs, f);
 		}
+		total_count++;
 	}
 
-	for (size_t i = 0; i < freqs.count; i++) {
+	for (size_t i = 0; i < freqs.size; i++) {
 		word_freq freq = freqs.items[i];
-		int n = (int) freq.word.count;
-		printf("|%.*s| => %d\n", n,freq.word.cstr, freq.count);
+		int n = (int)freq.word.size;
+		float p = (float)freq.count / total_count;
+		printf("|%.*s| => %d (%.5f)\n", n, freq.word.cstr, freq.count,
+		       p);
 	}
 
 	da_free(&buf);
